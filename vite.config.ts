858edb4d31486ce89d01e77b98ca0587c23cbfbd
4ster-light <4ster.dev@proton.me>
2026-05-31 12:12:47 +0200

    chore: add @types/node and fix Vite alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
   resolve: {
     alias: {
-      "@": "./src",
+      "@/": fileURLToPath(new URL("./src/", import.meta.url)),
     },
   },
 });
